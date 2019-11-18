bd0d0fc86af7be50dc717b2ad7b093371e0a5473
psyjc8 <psyjc8@exmail.nottingham.ac.uk>
2019-11-17 19:49:48 -0800

    modify size

--- interface.py
+++ interface.py
@@ -22,31 +22,31 @@ class Ui_Dialog(object):
 
     def setupUi(self, Dialog):
         Dialog.setObjectName("Dialog")
-        Dialog.resize(400, 300)
+        Dialog.setFixedSize(500, 500)
         Dialog.setStyleSheet("background-color: rgb(252, 230, 255);")
         self.lineEdit = QtWidgets.QLineEdit(Dialog)
-        self.lineEdit.setGeometry(QtCore.QRect(70, 60, 113, 21))
+        self.lineEdit.setGeometry(QtCore.QRect(80, 60, 125, 30))
         self.lineEdit.setStyleSheet("\n"
                                     "background-color: rgb(255, 255, 255);")
 
         self.lineEdit.setText("")
         self.lineEdit.setObjectName("lineEdit")
         self.textEdit = QtWidgets.QTextEdit(Dialog)
-        self.textEdit.setGeometry(QtCore.QRect(70, 150, 251, 111))
+        self.textEdit.setGeometry(QtCore.QRect(80, 180, 320, 240))
         self.textEdit.setStyleSheet("background-color: rgb(255, 255, 255);")
         self.textEdit.setObjectName("textEdit")
         self.pushButton = QtWidgets.QPushButton(Dialog)
-        self.pushButton.setGeometry(QtCore.QRect(220, 30, 113, 32))
+        self.pushButton.setGeometry(QtCore.QRect(280, 60, 113, 32))
         self.pushButton.setObjectName("pushButton")
         self.pushButton.setStyleSheet("\n"
                                       "background-color: rgb(97, 146, 255);")
         self.pushButton_2 = QtWidgets.QPushButton(Dialog)
-        self.pushButton_2.setGeometry(QtCore.QRect(220, 80, 113, 32))
+        self.pushButton_2.setGeometry(QtCore.QRect(280, 110, 113, 32))
         self.pushButton_2.setObjectName("pushButton_2")
         self.pushButton_2.setStyleSheet("\n"
                                         "background-color: rgb(253, 154, 126);")
         self.pushButton_3 = QtWidgets.QPushButton(Dialog)
-        self.pushButton_3.setGeometry(QtCore.QRect(60, 100, 130, 32))
+        self.pushButton_3.setGeometry(QtCore.QRect(80, 110, 140, 32))
         self.pushButton_3.setObjectName("pushButton_3")
         self.pushButton_3.setStyleSheet("\n"
                                         "background-color: rgb(156, 217, 173);")
