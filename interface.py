ef2f78256bdf5e8f695bec256ec5a07f5f7e3d9e
psyjc8 <psyjc8@exmail.nottingham.ac.uk>
2019-11-17 18:00:20 -0800

    change color

--- interface.py
+++ interface.py
@@ -8,27 +8,44 @@
 
 
 from PyQt5 import QtCore, QtWidgets
+import sys
 from pnn import analysis
 import preprocessing
 from preprocessing import Language
 
+
 class Ui_Dialog(object):
+
     def setupUi(self, Dialog):
         Dialog.setObjectName("Dialog")
         Dialog.resize(400, 300)
+        Dialog.setStyleSheet("background-color: rgb(252, 230, 255);")
         self.lineEdit = QtWidgets.QLineEdit(Dialog)
         self.lineEdit.setGeometry(QtCore.QRect(70, 60, 113, 21))
+        self.lineEdit.setStyleSheet("\n"
+                                    "background-color: rgb(255, 255, 255);")
+
         self.lineEdit.setText("")
         self.lineEdit.setObjectName("lineEdit")
         self.textEdit = QtWidgets.QTextEdit(Dialog)
         self.textEdit.setGeometry(QtCore.QRect(70, 150, 251, 111))
+        self.textEdit.setStyleSheet("background-color: rgb(255, 255, 255);")
         self.textEdit.setObjectName("textEdit")
         self.pushButton = QtWidgets.QPushButton(Dialog)
         self.pushButton.setGeometry(QtCore.QRect(220, 30, 113, 32))
         self.pushButton.setObjectName("pushButton")
+        self.pushButton.setStyleSheet("\n"
+                                      "background-color: rgb(97, 146, 255);")
         self.pushButton_2 = QtWidgets.QPushButton(Dialog)
         self.pushButton_2.setGeometry(QtCore.QRect(220, 80, 113, 32))
         self.pushButton_2.setObjectName("pushButton_2")
+        self.pushButton_2.setStyleSheet("\n"
+                                        "background-color: rgb(253, 154, 126);")
+        self.pushButton_3 = QtWidgets.QPushButton(Dialog)
+        self.pushButton_3.setGeometry(QtCore.QRect(60, 100, 130, 32))
+        self.pushButton_3.setObjectName("pushButton_3")
+        self.pushButton_3.setStyleSheet("\n"
+                                        "background-color: rgb(156, 217, 173);")
 
         self.retranslateUi(Dialog)
         self.pushButton.clicked.connect(self.startAnalysis)
@@ -49,6 +66,8 @@ class Ui_Dialog(object):
         Dialog.setWindowTitle(_translate("Dialog", "Language Guessing"))
         self.pushButton.setText(_translate("Dialog", "Search"))
         self.pushButton_2.setText(_translate("Dialog", "Cancel"))
+        self.pushButton_3.setText(_translate(
+            "Dialog", "Help: Check symbol"))
 
 
 if __name__ == "__main__":
